4f71b4bcf2b19c86c6666510835a0c26f6c11406
Radel <latyp47@mail.ru>
2022-06-30 22:25:32 +0300

    Задание с массивом

---
+++ w1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задан массив");
+string [] numbers = new string [] {"1","2","3","4","5"};
+for(byte i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+   Console.WriteLine("Новый массив" );
+   for(byte i = 0; i < 3; i++)
+    {
+       Console.WriteLine(numbers[i]);
+    }
+    
